acffd3952699a9e3cdafb1eb93da0b13001c047a
eriko <gwifloria@outlook.com>
2025-07-14 15:53:53 +0800

    feat: improve homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,14 @@ export default function Home() {
     <StyleProvider hashPriority="high">
       <AuthProvider>
         <main className="flex min-h-screen flex-col items-center justify-between ">
-          <ParallaxFavs />
-          <HeroSection />
-          <PersonalIntroduction />
+          <div className="w-full max-w-6xl bg-white rounded-[2.5rem] shadow-xl p-2 sm:p-6 md:p-12 lg:p-16 my-8 border-2 border-mint-100/40">
+            <ParallaxFavs />
+            <HeroSection />
+          </div>
+          <div className="w-full max-w-6xl bg-white rounded-[2.5rem] shadow-md p-2 sm:p-6 md:p-12 lg:p-16 my-8 border border-mint-100/30">
+            <PersonalIntroduction />
+          </div>
+
           <SayHiButton />
         </main>
       </AuthProvider>
