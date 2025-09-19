8dc93094e8d8f53cedbf897a38a29765aa4da4e2
eriko <gwifloria@outlook.com>
2025-09-19 19:34:52 +0800

    fix: test

--- next.config.mjs
+++ next.config.mjs
@@ -74,7 +74,7 @@ const nextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
+            value: "public, immutable",
           },
           { key: "Vary", value: "Accept" },
         ],
